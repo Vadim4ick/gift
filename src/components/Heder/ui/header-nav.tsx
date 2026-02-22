8cfa2c63b9cdf899787f38ba599d0819ed4d883d
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2026-02-22 11:44:14 +0300

    feat: оптимизация ререндера в хедера + новый ci/cd

--- src/components/Heder/ui/header-nav.tsx
+++ src/components/Heder/ui/header-nav.tsx
@@ -1,13 +1,30 @@
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { memo } from "react";
 import { NavLinkProps } from "../model/types";
-import { NavLink } from "./navLink";
 
 const HeaderNav = memo(({ data }: { data: NavLinkProps[] }) => {
+  const pathname = usePathname();
+
   return (
-    <nav className="hidden lg:flex items-center gap-8">
-      {data.map((item) => (
-        <NavLink key={item.href} {...item} />
-      ))}
+    <nav className="hidden md:flex items-center gap-5 lg:gap-8">
+      {data.map((item) => {
+        const isActive = pathname === item.href;
+
+        return (
+          <Link
+            key={item.href}
+            href={item.href}
+            className={`text-lg text-gray-700 hover:text-purple-600 transition-colors ${
+              isActive
+                ? "font-bold bg-linear-to-r from-purple-600 via-pink-600 to-blue-600 bg-clip-text text-transparent"
+                : ""
+            } `}
+          >
+            {item.label}
+          </Link>
+        );
+      })}
     </nav>
   );
 });
--- src/components/Heder/ui/mobile-nav.tsx
+++ src/components/Heder/ui/mobile-nav.tsx
@@ -4,44 +4,42 @@ import Link from "next/link";
 import { memo } from "react";
 import { NavLinkProps } from "../model/types";
 import { Logo } from "./logo";
+import { usePathname } from "next/navigation";
 
 type MobileNavProps = {
   data: NavLinkProps[];
   onClose: () => void;
-  pathname?: string;
 };
 
-export const MobileNav = memo(
-  ({ data: navItems, onClose, pathname }: MobileNavProps) => {
-    return (
-      <div className="flex flex-col bg-background">
-        <div className="px-6 py-4 border-b border-purple-50">
-          <Logo onClick={onClose} />
-        </div>
-
-        <nav className="flex flex-col h-dvh w-full space-y-4 p-6">
-          {navItems.map((item) => {
-            const isActive = pathname === item.href;
-
-            return (
-              <Link
-                key={item.href}
-                href={item.href}
-                onClick={onClose}
-                className={`text-2xl py-3 transition-all duration-200 active:scale-95 m-0 ${
-                  isActive
-                    ? "font-bold bg-linear-to-r from-purple-600 via-pink-600 to-blue-600 bg-clip-text text-transparent"
-                    : "text-gray-600 hover:text-purple-600"
-                }`}
-              >
-                {item.label}
-              </Link>
-            );
-          })}
-        </nav>
+export const MobileNav = memo(({ data: navItems, onClose }: MobileNavProps) => {
+  const pathname = usePathname();
+  return (
+    <div className="flex flex-col h-screen bg-background overflow-hidden">
+      <div className="px-6 py-4 border-b border-purple-50 flex-none">
+        <Logo onClick={onClose} />
       </div>
-    );
-  },
-);
+
+      <nav className="flex flex-col flex-1 p-6 space-y-2 overflow-y-auto">
+        {navItems.map((item) => {
+          const isActive = pathname === item.href;
+          return (
+            <Link
+              key={item.href}
+              href={item.href}
+              onClick={onClose}
+              className={`text-2xl py-3 transition-all duration-200 active:scale-95 ${
+                isActive
+                  ? "font-bold bg-linear-to-r from-purple-600 via-pink-600 to-blue-600 bg-clip-text text-transparent"
+                  : "text-gray-600 hover:text-purple-600"
+              }`}
+            >
+              {item.label}
+            </Link>
+          );
+        })}
+      </nav>
+    </div>
+  );
+});
 
 MobileNav.displayName = "MobileNav";
