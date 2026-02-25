606240a6d864b4344761c47b0199198b5b841095
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2026-02-25 17:31:38 +0300

    fix sitemam routes generator

--- src/app/(sitemaps)/pages-sitemap.xml/route.ts
+++ src/app/(sitemaps)/pages-sitemap.xml/route.ts
@@ -1,23 +1,18 @@
-import { getAllPostsMeta } from "@/shared/lib/blog";
 import { getServerSideSitemap } from "next-sitemap";
 import { unstable_cache } from "next/cache";
 
 const getPagesSitemap = unstable_cache(async () => {
   const SITE_URL =
     process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
-
-  const posts = getAllPostsMeta();
-  const dateFallback = new Date().toISOString();
-
-  const sitemap = posts
-    ? posts
-        .filter((post) => Boolean(post?.slug))
-        .map((post) => ({
-          loc: `${SITE_URL}/blog/${post?.slug}`,
-          lastmod: post.date || dateFallback,
-        }))
-    : [];
-  return sitemap;
+  const defaultSitemap = [
+    {
+      loc: `${SITE_URL}/`,
+    },
+    {
+      loc: `${SITE_URL}/form`,
+    },
+  ];
+  return [...defaultSitemap];
 });
 
 export async function GET() {
