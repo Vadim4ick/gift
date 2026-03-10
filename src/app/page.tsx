43649f91793a48be4cb30dcfda31adf918d88fa1
Ruslan Kharisov <ruslan.kharisov@gmail.com>
2026-03-10 20:05:09 +0300

    feat: banner slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { Hero } from "@/modules/home/Hero";
 import { HowItWorks } from "@/modules/home/How-it-works";
 import { CtaBaner } from "@/modules/home/CtaBaner";
 import { Metadata } from "next";
+import { CarouselWrapper } from "@/components/CarouselWrapper";
+import { sliderData } from "@/shared/const/slider-data";
+import { HeroSlide } from "@/components/HeroSlide";
 
 export const metadata: Metadata = {
   title: "Идеальный подарок с ии бесплатно - GiftGenius поможет за минуту",
@@ -13,6 +16,12 @@ export default function GiftGenius() {
   return (
     <div className="mt-(--header-height) grow">
       <Hero />
+
+      <CarouselWrapper aspectClassName="aspect-[3/4] lg:aspect-[3/1]">
+        {sliderData.map((slide, index) => (
+          <HeroSlide key={index} {...slide} />
+        ))}
+      </CarouselWrapper>
 
       <HowItWorks />
 
--- src/shared/ui/button.tsx
+++ src/shared/ui/button.tsx
@@ -1,6 +1,6 @@
 import * as React from "react";
-import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
+import { Slot } from "@radix-ui/react-slot";
 
 import { cn } from "@/shared/lib/utils";
 
@@ -11,10 +11,27 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-linear-to-r from-purple-600 to-pink-600 text-white rounded-full hover:shadow-lg hover:scale-105 transition-all",
+        outline:
+          "border-border bg-background hover:bg-muted hover:text-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 aria-expanded:bg-muted aria-expanded:text-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80 aria-expanded:bg-secondary aria-expanded:text-secondary-foreground",
+        ghost:
+          "hover:bg-muted hover:text-foreground dark:hover:bg-muted/50 aria-expanded:bg-muted aria-expanded:text-foreground",
+        destructive:
+          "bg-destructive/10 hover:bg-destructive/20 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/20 text-destructive focus-visible:border-destructive/40 dark:hover:bg-destructive/30",
+        link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
+        xs: "h-6 gap-1 rounded-[min(var(--radius-md),10px)] px-2 text-xs in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3",
+        sm: "h-7 gap-1 rounded-[min(var(--radius-md),12px)] px-2.5 text-[0.8rem] in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3.5",
         lg: "h-12 px-6 has-[>svg]:px-4",
+        icon: "size-8",
+        "icon-xs":
+          "size-6 rounded-[min(var(--radius-md),10px)] in-data-[slot=button-group]:rounded-lg [&_svg:not([class*='size-'])]:size-3",
+        "icon-sm":
+          "size-7 rounded-[min(var(--radius-md),12px)] in-data-[slot=button-group]:rounded-lg",
+        "icon-lg": "size-9",
       },
     },
     defaultVariants: {
